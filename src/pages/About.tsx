0e787f66bca0803a00fe8bea7049d46d5e787808
Abror <abrorrr15@gmail.com>
2024-05-25 18:53:43 +0500

    2

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,13 +1,13 @@
-import { useEffect, useRef } from 'react';
-import Logo from '../components/Logo';
-import { RxAvatar } from 'react-icons/rx';
-import { FaGithub, FaPhone, FaTelegram } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
-import { BiLogoInstagram } from 'react-icons/bi';
-import { SwiperSlide, Swiper } from 'swiper/react';
-import { Autoplay } from 'swiper/modules';
-import { useTranslation } from 'react-i18next';
-
+import { useEffect, useRef } from "react";
+import Logo from "../components/Logo";
+import { RxAvatar } from "react-icons/rx";
+import { FaGithub, FaPhone, FaTelegram } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { BiLogoInstagram } from "react-icons/bi";
+import { SwiperSlide, Swiper } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import { useTranslation } from "react-i18next";
+// Starting without a computer, only a smartphone.
 function About() {
   const { t } = useTranslation();
   const contentRef = useRef<HTMLDivElement | null>(null);
@@ -17,15 +17,15 @@ function About() {
       (entries) => {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
-            entry.target.classList.add('visible');
+            entry.target.classList.add("visible");
             observer.unobserve(entry.target);
           }
         });
       },
-      { threshold: 0.1 },
+      { threshold: 0.1 }
     );
 
-    const contentElements = contentRef.current?.querySelectorAll('.fade-in');
+    const contentElements = contentRef.current?.querySelectorAll(".fade-in");
     contentElements?.forEach((el) => {
       observer.observe(el);
     });
@@ -38,11 +38,14 @@ function About() {
   }, []);
 
   return (
-    <div className="min-h-screen overflow-y-auto custom-scrollbar" ref={contentRef}>
+    <div
+      className="min-h-screen overflow-y-auto custom-scrollbar"
+      ref={contentRef}
+    >
       <header className="flex items-center justify-between font-medium text-white fade-in">
         <div className="flex items-center m-[2em] mb-[1em] xl:mb-[1.3em] xl:m-[1.5em] 2xl:m-[1em] 2xl:mb-[1.5em] text-3xl font-medium text-white fade-in">
           <RxAvatar size={35} />
-          <span className="ml-2">{t('aboutMe')}</span>
+          <span className="ml-2">{t("aboutMe")}</span>
         </div>
         <Logo />
       </header>
@@ -50,19 +53,30 @@ function About() {
       <main className="px-8 ml-3 md:px-32 text-start bg-[#ffffff9f] rounded-lg shadow-lg fade-in">
         <section className="my-8">
           <h1 className="lg:text-4xl 2xl:text-2xl font-bold mb-4">
-            {t('introduction')}
+            {t("introduction")}
           </h1>
           <p className="lg:text-lg 2xl:text-md">
-            {t('hiIm')} <b>{t("AbrorXoliqov")}</b>, {t('apassionatesoftwareengineer')} <b><i>(front-end {t("developer")})</i></b> {t("withastrongbackgroundinwebdevelopmentandakeeninterestinnewtechnologies.")}
+            {t("hiIm")} <b>{t("AbrorXoliqov")}</b>,{" "}
+            {t("apassionatesoftwareengineer")}{" "}
+            <b>
+              <i>(front-end {t("developer")})</i>
+            </b>{" "}
+            {t(
+              "withastrongbackgroundinwebdevelopmentandakeeninterestinnewtechnologies."
+            )}
           </p>
         </section>
         <hr className="w-full" />
         <section className="my-8">
           <h1 className="lg:text-4xl 2xl:text-2xl font-bold mb-4">
-            {t('background')}
+            {t("background")}
           </h1>
           <p className="lg:text-lg 2xl:text-md">
-            {t("I am a student at the National University of Uzbekistan Tashkent. I have worked on various projects, both independently and as part of a team, and I am currently employed at")} <b>IST Telekom</b>{t(". Down below you can see photos of my university and office.")}
+            {t(
+              "I am a student at the National University of Uzbekistan Tashkent. I have worked on various projects, both independently and as part of a team, and I am currently employed at"
+            )}{" "}
+            <b>IST Telekom</b>
+            {t(". Down below you can see photos of my university and office.")}
           </p>
           <br />
           <div className="grid grid-cols-2 grid-rows-1 place-items-center">
@@ -96,10 +110,18 @@ function About() {
         <hr className="w-full" />
         <section className="my-8">
           <h1 className="lg:text-4xl 2xl:text-2xl font-bold mb-4">
-            {t('studyJourney')}
+            {t("studyJourney")}
           </h1>
           <p className="lg:text-lg 2xl:text-md">
-            {t("My programming journey began on ")}<u>{t("January 12, 2024")}</u>{t(". Without access to offline courses or a personal teacher, I relied solely on")} <b>YouTube</b> {t("lessons to learn programming. Starting without a computer, only a smartphone. I faced and overcame numerous challenges. This experience not only honed my problem-solving skills but also instilled a strong sense of resilience and self-discipline, setting me apart from other applicants. In addition, I have taken IELTS in 2023 year studying on my own too, score you can see below in the photo.")}
+            {t("My programming journey began on ")}
+            <u>{t("January 12, 2024")}</u>
+            {t(
+              ". Without access to offline courses or a personal teacher, I relied solely on"
+            )}{" "}
+            <b>YouTube</b>{" "}
+            {t(
+              "lessons to learn programming. I faced and overcame numerous challenges. This experience not only honed my problem-solving skills but also instilled a strong sense of resilience and self-discipline, setting me apart from other applicants. In addition, I have taken IELTS in 2023 year studying on my own too, score you can see below in the photo."
+            )}
           </p>
           <br />
           <img src="ielts.png" alt="ielts" className="w-fit rounded-[15px]" />
@@ -107,37 +129,45 @@ function About() {
         <hr className="w-full" />
         <section className="my-8">
           <h1 className="lg:text-4xl 2xl:text-2xl font-bold mb-4">
-            {t('skills')}
+            {t("skills")}
           </h1>
           <p className="lg:text-lg 2xl:text-md">
-            {t("I am proficient in HTML, CSS, JavaScript, and have experience with frameworks and libraries like React, Redux and React-query. I am also familiar with TypeScript, Git, and various development tools.")}
+            {t(
+              "I am proficient in HTML, CSS, JavaScript, and have experience with frameworks and libraries like React, Redux and React-query. I am also familiar with TypeScript, Git, and various development tools."
+            )}
           </p>
         </section>
         <hr className="w-full" />
         <section className="my-8">
           <h1 className="lg:text-4xl 2xl:text-2xl font-bold mb-4">
-            {t('currentWork')}
+            {t("currentWork")}
           </h1>
           <p className="lg:text-lg 2xl:text-md">
-            {t("Currently, I am working on several personal projects, exploring new technologies like Vue.js and Next js, and contributing to open source projects.")}
+            {t(
+              "Currently, I am working on several personal projects, exploring new technologies like Vue.js and Next js, and contributing to open source projects."
+            )}
           </p>
         </section>
         <hr className="w-full" />
         <section className="my-8">
           <h1 className="lg:text-4xl 2xl:text-2xl font-bold mb-4">
-            {t('hobbiesAndInterests')}
+            {t("hobbiesAndInterests")}
           </h1>
           <p className="lg:text-lg 2xl:text-md">
-            {t("In my free time, I enjoy coding and reading tech blogs. I also love traveling and exploring new cultures.")}
+            {t(
+              "In my free time, I enjoy coding and reading tech blogs. I also love traveling and exploring new cultures."
+            )}
           </p>
         </section>
         <hr className="w-full" />
         <section className="my-8">
           <h1 className="lg:text-4xl 2xl:text-2xl font-bold mb-4">
-            {t('contact')}
+            {t("contact")}
           </h1>
           <p className="lg:text-lg 2xl:text-md">
-            {t("Feel free to reach out to me via email at ")}<b>abrorrr15@gmail.com</b>{t(" or connect with me on social media:")}
+            {t("Feel free to reach out to me via email at ")}
+            <b>abrorrr15@gmail.com</b>
+            {t(" or connect with me on social media:")}
           </p>
           <div className="flex gap-4 mt-4">
             <a
