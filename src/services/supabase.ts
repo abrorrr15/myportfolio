6e0d0a6f676975adb5a11211186e1b4b16349dac
abrorrr15 <abrorrr15@gmail.com>
2024-09-01 23:54:45 +0500

    fix

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabaseUrl = "https://egrbzefgcancbhqmodfm.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVncmJ6ZWZnY2FuY2JocW1vZGZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYwNTYzODUsImV4cCI6MjAzMTYzMjM4NX0.HfeLQTkRO8O_c2b_a1KBzkogOandZiv9DQ2HPCJ5y-M";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVncmJ6ZWZnY2FuY2JocW1vZGZtIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxNjA1NjM4NSwiZXhwIjoyMDMxNjMyMzg1fQ.wdBn6Nw-MsANeD7s3-eFoUd581zekE1-SFY9215xXO8";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
